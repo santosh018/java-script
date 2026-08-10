c67a50d49ca1c71aa0190227386a86a2c0dfec71
santosh018 <skbhandariii07@gmail.com>
2026-08-10 11:53:43 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,21 @@
+
+const product = {
+    title: "ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+
+console.log(product);
+
+
+const profile = {
+    username: "@shradhakhapra",
+    isFollow: true,
+    followers: 569,
+    following: 4
+    
+    
+};
+
+console.log(profile);
